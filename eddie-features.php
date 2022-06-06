56175ee180671407297ffb1eb82e1f2c02aa5d8c
edwardosei <edward.osei.nyarko@gmail.com>
2022-06-06 17:04:19 +0000

    Added Plugin Header Requirement

---
+++ eddie-features.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       Eddie Featues
+ * Plugin URI:        https://github.com/nana-yaw
+ * Description:       Adds Gutenberg Blocks.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Edward Osei-Nyarko
+ * Author URI:        https://www.linkedin.com/in/edward-osei-nyarko-5789b3118/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       eddie-features
+ * Domain Path:       /languages
+ */
